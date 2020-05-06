21317611b6a44eae0263939a81d3b4148b9ce80c
alvaro <laheraalvaro@gmail.com>
2020-05-06 17:22:51 +0200

    testPersonaStringStringString

---
+++ src/ed/examen/modelo/CursoTest.java
@@ -0,0 +1,34 @@
+package ed.examen.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+
+	@Test
+	void testEliminarAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAniadirAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaRegistrado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNumeroAlumnos() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/ed/examen/modelo/PersonaTest.java
@@ -0,0 +1,47 @@
+package ed.examen.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+	Persona p = new Persona("1807508D", "Alvaro", "Martinez");
+
+	
+	@Test
+	void testPersonaStringStringString() {
+		 assertEquals("1807508D", p.getDni());
+	}
+
+	@Test
+	void testGetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetApellido1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetApellido1() {
+		fail("Not yet implemented");
+	}
+
+}
